fa225ee9a8ddd47b6cd7a6ad58d7e57446eed878
Craig Gidney <craiggidney@google.com>
2018-02-22 10:42:52 -0800

    undo copy tweaks

--- cirq/examples/supremacy.py
+++ cirq/examples/supremacy.py
@@ -17,85 +17,85 @@ import random
 import cirq
 import cirq.google
 
-def main():
 
-    def generate_supremacy_circuit(device, cz_depth):
+def generate_supremacy_circuit(device, cz_depth):
 
-        circuit = cirq.Circuit()
+    circuit = cirq.Circuit()
 
-        i = 0
-        while cz_depth > 0:
-            cz_layer = make_cz_layer(device, i)
-            if cz_layer:
-                circuit.append(make_random_single_qubit_op_layer(device))
-                circuit.append(make_cz_layer(device, i))
-                cz_depth -= 1
-            i += 1
+    i = 0
+    while cz_depth > 0:
+        cz_layer = make_cz_layer(device, i)
+        if cz_layer:
+            circuit.append(make_random_single_qubit_op_layer(device))
+            circuit.append(make_cz_layer(device, i))
+            cz_depth -= 1
+        i += 1
 
-        circuit.append(make_random_single_qubit_op_layer(device))
-        circuit.append(cirq.google.XmonMeasurementGate().on(q)
-                       for q in device.qubits)
+    circuit.append(make_random_single_qubit_op_layer(device))
+    circuit.append(cirq.google.XmonMeasurementGate().on(q)
+                   for q in device.qubits)
 
-        return circuit
-
-    def make_random_single_qubit_op_layer(device):
-        vals = [random.randint(0, 7) / 4.0 for _ in device.qubits]
-        phases = [random.randint(0, 7) / 4.0 for _ in device.qubits]
-        return [
-            cirq.google.ExpWGate(half_turns=angle, axis_half_turns=axis).on(q)
-            for angle, axis, q in zip(vals, phases, device.qubits)
-            if angle
-        ]
+    return circuit
 
 
-    def make_cz_layer(device, layer_index):
-        """
-        Each layer index corresponds to a shift/transpose of this CZ pattern:
+def make_random_single_qubit_op_layer(device):
+    vals = [random.randint(0, 7) / 4.0 for _ in device.qubits]
+    phases = [random.randint(0, 7) / 4.0 for _ in device.qubits]
+    return [
+        cirq.google.ExpWGate(half_turns=angle, axis_half_turns=axis).on(q)
+        for angle, axis, q in zip(vals, phases, device.qubits)
+        if angle
+    ]
 
-            ●───●   ●   ●   ●───●   ●   ● . . .
 
-            ●   ●   ●───●   ●   ●   ●───● . . .
+def make_cz_layer(device, layer_index):
+    """
+    Each layer index corresponds to a shift/transpose of this CZ pattern:
 
-            ●───●   ●   ●   ●───●   ●   ● . . .
+        ●───●   ●   ●   ●───●   ●   ● . . .
 
-            ●   ●   ●───●   ●   ●   ●───● . . .
+        ●   ●   ●───●   ●   ●   ●───● . . .
 
-            ●───●   ●   ●   ●───●   ●   ● . . .
+        ●───●   ●   ●   ●───●   ●   ● . . .
 
-            ●   ●   ●───●   ●   ●   ●───● . . .
-            .   .   .   .   .   .   .   . .
-            .   .   .   .   .   .   .   .   .
-            .   .   .   .   .   .   .   .     .
+        ●   ●   ●───●   ●   ●   ●───● . . .
 
-        Labelled edges, showing the exact index-to-CZs mapping (mod 8):
+        ●───●   ●   ●   ●───●   ●   ● . . .
 
-             ●─0─●─2─●─4─●─6─●─0─. . .
-            1│  5│  1│  5│  1│
-             ●─4─●─6─●─0─●─2─●─4─. . .
-            3│  7│  3│  7│  3│
-             ●─0─●─2─●─4─●─6─●─0─. . .
-            5│  1│  5│  1│  5│
-             ●─4─●─6─●─0─●─2─●─4─. . .
-            7│  3│  7│  3│  7│
-             ●─0─●─2─●─4─●─6─●─0─. . .
-            1│  5│  1│  5│  1│
-             .   .   .   .   .   .
-             .   .   .   .   .     .
-             .   .   .   .   .       .
+        ●   ●   ●───●   ●   ●   ●───● . . .
+        .   .   .   .   .   .   .   . .
+        .   .   .   .   .   .   .   .   .
+        .   .   .   .   .   .   .   .     .
 
-        Note that, for small devices, some layers will be empty because the layer
-        only contains edges not present on the device.
-        """
+    Labelled edges, showing the exact index-to-CZs mapping (mod 8):
 
-        dir_x = layer_index % 2
-        dir_y = 1 - dir_x
-        shift = (layer_index >> 1) % 4
+         ●─0─●─2─●─4─●─6─●─0─. . .
+        1│  5│  1│  5│  1│
+         ●─4─●─6─●─0─●─2─●─4─. . .
+        3│  7│  3│  7│  3│
+         ●─0─●─2─●─4─●─6─●─0─. . .
+        5│  1│  5│  1│  5│
+         ●─4─●─6─●─0─●─2─●─4─. . .
+        7│  3│  7│  3│  7│
+         ●─0─●─2─●─4─●─6─●─0─. . .
+        1│  5│  1│  5│  1│
+         .   .   .   .   .   .
+         .   .   .   .   .     .
+         .   .   .   .   .       .
 
-        for q in device.qubits:
-            q2 = cirq.google.XmonQubit(q.x + dir_x, q.y + dir_y)
-            if q2 not in device.qubits:
-                continue  # This edge isn't on the device.
-            if (q.x * (2 - dir_x) + q.y * (2 - dir_y)) % 4 != shift:
-                continue  # No CZ along this edge for this layer.
+    Note that, for small devices, some layers will be empty because the layer
+    only contains edges not present on the device.
+    """
 
-            yield cirq.google.Exp11Gate().on(q, q2)
+    dir_x = layer_index % 2
+    dir_y = 1 - dir_x
+    shift = (layer_index >> 1) % 4
+
+    for q in device.qubits:
+        q2 = cirq.google.XmonQubit(q.x + dir_x, q.y + dir_y)
+        if q2 not in device.qubits:
+            continue  # This edge isn't on the device.
+        if (q.x * (2 - dir_x) + q.y * (2 - dir_y)) % 4 != shift:
+            continue  # No CZ along this edge for this layer.
+
+        yield cirq.google.Exp11Gate().on(q, q2)
--- cirq/sim/google/xmon_simulator.py
+++ cirq/sim/google/xmon_simulator.py
@@ -359,32 +359,3 @@ class TrialResult(cirq.study.TrialResult):
         sorted_bitstrings = sorted(keyed_bitstrings, key=lambda e: e[0])
         return '\n'.join('{}: {}'.format(repr(key), val)
                          for key, val in sorted_bitstrings)
-
-
-def main():
-
-        import cirq
-
-        # Define a qubit.
-        qubit = cirq.google.XmonQubit(0, 0)
-
-        # Create a circuit (qubits start in the |0> state).
-        circuit = cirq.Circuit()
-        circuit.append([
-            # Square root of NOT.
-            cirq.X(qubit)**0.5,
-
-            # Measurement.
-            cirq.MeasurementGate('result').on(qubit)
-        ])
-        print("Circuit:")
-        print(cirq.to_ascii(circuit))
-
-        # Now simulate the circuit and print out the measurement result.
-        simulator = cirq.sim.google.xmon_simulator.Simulator()
-        results = []
-        for _ in range(10):
-            _, result = simulator.run(circuit)
-            results.append('1' if result.measurements['result'][0] else '0')
-        print("Simulated measurement results:")
-        print(''.join(results))
